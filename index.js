2aaa1337a437e1608d3ec6726e4447f5dec5e05c
김진선 <lper11@naver.com>
2021-03-17 01:29:56 +0900

    패스워드 암호화 DB 저장

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const port = 5000
 
 const { User } = require("./models/User");
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const config = require('./config/key');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect(config.mongoURI, {
@@ -21,14 +21,16 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello World! 감사합니다~'))
 
 app.post('/register', (req, res) => {
-
+    console.log(req.body);
     // 회원가입에 필요한 정보를 client에서 가져오면 데이터베이스에 넣어줌
     const user = new User(req.body)
+    console.log(user);
 
-    user.save((err, doc) => {
+    user.save((err, userInfo) => {
         if(err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
+            
         })
     })
 })
