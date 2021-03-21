6823be4dbf36df678edf42b14a7d0e5a9ff5527d
김진선 <lper11@naver.com>
2021-03-22 00:43:45 +0900

    auth, 로그아웃 기능 추가

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10
-
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -51,6 +51,42 @@ userSchema.pre('save', function (next) {
         next()
     }
 })
+
+//비밀번호 맞는지 체크하는 메서드
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch) {
+        if(err) return cb(err);
+        cb(null,isMatch);
+    })
+}
+
+//비밀번호가 맞다면 토큰 생성하는 메서드
+userSchema.methods.generateToken = function(cb) {
+    
+    var user = this;
+    //jsonwebtoken을 이용하여 token 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    })   
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    // 토큰 decode
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 ID를 이용하여 유저를 찾은 다음 클라이언트에서 가져온 TOKEN과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decoded, "token": token }, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
