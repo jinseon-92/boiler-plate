d1b8783743fb7b9fdcf60f0ef0b52fea5657018e
김진선 <lper11@naver.com>
2021-03-26 00:00:58 +0900

    redux & 로그인 페이지 생성

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,6 @@ function App() {
   return (
     <Router>
       <div>
-        
-        <hr />
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
